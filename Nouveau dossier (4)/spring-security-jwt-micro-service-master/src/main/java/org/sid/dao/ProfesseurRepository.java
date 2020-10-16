1976bac44970975dbbf20774811ed5d200f089ec
itsatrap <zakaria.lachgar@um5s.net.ma>
2020-10-16 22:11:43 +0100

    version 3

--- Nouveau dossier (4)/spring-security-jwt-micro-service-master/src/main/java/org/sid/dao/ProfesseurRepository.java
+++
@@ -1,8 +0,0 @@
-package org.sid.dao;
-
-import org.sid.entities.Professeur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfesseurRepository extends JpaRepository<Professeur,Long> {
-    public Professeur findByUsername(String username);
-}
